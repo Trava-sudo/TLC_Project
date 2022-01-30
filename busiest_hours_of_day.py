1c7eb944f4d7ba6ec944ec0c8ebc27ccc13f54c6
francescotravaglini <82157032+Trava-sudo@users.noreply.github.com>
2022-01-30 14:47:50 +0100

    Update busiest_hours_of_day.py

--- busiest_hours_of_day.py
+++ busiest_hours_of_day.py
@@ -29,9 +29,16 @@ for j, name_piece in enumerate (datasets):
         sql_query_busiest_hours += table[0] + '` group by DATE_FORMAT(pickup_datetime, \'%H\') order by Counts desc;'  
         cur.execute(sql_query_busiest_hours)
         busiest_hours = cur.fetchall()
+### busiest_hours is an element such as: busiest_hours = (('10',45559),('09',293483),('03',38664),...  where there are 
+### as many elements as up to 23, for every hour of the day.
+        
+        
         for m, hour in enumerate (busiest_hours):
+            #### hour[0] is the element corresponding to the hour of the day, while hour[1] is the number of counts for that hour of day
             instances[int(hour[0])] += hour[1]
         
+### In this next lines the hour with the highest counts is extracted as a value and then the counts for that hour is lower to 0,
+### so that there is no inconsistency given from the pop() method that change the index of the hour
 
     first_busiest_hour_overall = instances.index(max(instances))
     instances[first_busiest_hour_overall] -= max(instances)
