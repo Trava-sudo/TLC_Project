9f98fcb71d72c4772c94dbc65f9e066a73267f3d
francescotravaglini <82157032+Trava-sudo@users.noreply.github.com>
2022-01-30 10:49:41 +0100

    Update import_data_to_database.py

--- import_data_to_database.py
+++ import_data_to_database.py
@@ -44,7 +44,7 @@ while current_date <= end_date:
 
 ### If one decides to, here are colled the functions to write the data into parquet and csv files
 
-            write_to_csv(data, folder_path='/path/to/folder', name_of_table)
+            is_csv_file = write_to_csv(data, folder_path='/path/to/folder', name_of_table)
 
 #### From here to the end the code reiterates the same process for data coreesponding to the datetime.date defined in the elif condition. These condition 
 #### where defined looking at the data: in 2013 green taxis's data were added to the yellow ones. In 2015 the for-hire vehicles were added. 
@@ -61,7 +61,7 @@ while current_date <= end_date:
                 data = pd.concat([data, chunk], ignore_index=True)
             types_table = infer_types(dataframe= data, types_array_element= types_table_alter[i])
             create_and_fill_dbTable(data, name_of_table, types_table)
-            write_to_csv(data, folder_path='/path/to/folder', name_of_table)
+            is_csv_file = write_to_csv(data, folder_path='/path/to/folder', name_of_table)
 
     elif (current_date < dt.date(2019,1,1) and current_date >= dt.date(2015,8,1)):
         for i, item in enumerate (datasets_url[0:3]):
@@ -72,7 +72,7 @@ while current_date <= end_date:
                 data = pd.concat([data, chunk], ignore_index=True)
             types_table = infer_types(dataframe= data, types_array_element= types_table_alter[i])
             create_and_fill_dbTable(data, name_of_table, types_table)
-            write_to_csv(data, folder_path='/path/to/folder', name_of_table)
+            is_csv_file = write_to_csv(data, folder_path='/path/to/folder', name_of_table)
     elif (current_date < dt.date(2019,2,1) and current_date >= dt.date(2019,1,1)):
         datasets_url[2] = "https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_"   ## Redefinition of the URL for the for-hire vehicles as anticipated
         for i, item in enumerate (datasets_url[0:3]):
@@ -83,7 +83,7 @@ while current_date <= end_date:
                 data = pd.concat([data, chunk], ignore_index=True)
             types_table = infer_types(dataframe= data, types_array_element= types_table_alter[i])
             create_and_fill_dbTable(data, name_of_table, types_table)
-            write_to_csv(data, folder_path='/path/to/folder', name_of_table)
+            is_csv_file = write_to_csv(data, folder_path='/path/to/folder', name_of_table)
     else:
         for i, item in enumerate (datasets_url):
             url = item + current_date.strftime("%Y-%m") + ".csv"
@@ -93,7 +93,7 @@ while current_date <= end_date:
                 data = pd.concat([data, chunk], ignore_index=True)
             types_table = infer_types(dataframe= data, types_array_element= types_table_alter[i])
             create_and_fill_dbTable(data, name_of_table, types_table)
-            write_to_csv(data, folder_path='/path/to/folder', name_of_table)
+            is_csv_file = write_to_csv(data, folder_path='/path/to/folder', name_of_table)
     
     current_date += relativedelta(months=1)
 
